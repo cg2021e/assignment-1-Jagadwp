19c947cb6e016cee288001986075078da7a45f87
Jagad Wijaya Purnomo <Jagadpurnomo26@gmail.com>
2021-10-06 00:00:05 +0700

    feat: add 2 eraser webgl object + animation

--- main.js
+++ main.js
@@ -1,112 +1,130 @@
-function main(){
-    var canvas = document.getElementById("Canvas");
-    var gl = canvas.getContext("webgl");
+function main() {
+    var canvas = document.getElementById('myCanvas');
+    var gl = canvas.getContext('webgl');
 
-    var leftVertices = [
-        // UNTUK MEMBUATNYA MENJADI TERCERMIN COBA PAKAI RUMUS x - 2 * (x + 0.5), dengan cermin di sumbu y = 0.5 (titik tengah)
-
-        //bagian atas
-        // segitiga 1 hitam kiri
-        0, 0, 0.235, 0.235,     ,  
-        -0.062, 0.3,  0.235, 0.235, 0.196,
-        -0.29, 0.3,  0.235, 0.235, 0.196, 
-
-        //segitiga 2 hitam kanan
-        0, 0, 0.235, 0.235, 0.196,  
-        -0.29, 0.3,  0.235, 0.235, 0.196, 
-        -0.26, 0, 0.235, 0.235, 0.196,
-
-        // sarung penghapus segitiga kiri bawah
-        -0.29, 0.33, 0.737, 0.682,0.580,
-        -0.26, 0, 0.737, 0.682,0.580,
-        -0.6, 0, 0.737, 0.682,0.580,
-
-        //sarung penghapus segitiga atas 
-        -0.29, 0.33, 0.737, 0.682,0.580,
-        -0.95, 0.33, 0.737, 0.682,0.580,
-        -0.6, 0, 0.737, 0.682,0.580,
+    var vertices1 = [
+        // ATAS KARET PENGHAPUS KIRI
+        -0.9262413741382, -0.0801076682883  ,0.27, 0.26, 0.26,     // D
+        -0.7471120316662, 0.1049398953577   ,0.27, 0.26, 0.26,     // G
+        -0.8627372789808, 0.1049398953577   ,0.27, 0.26, 0.26,     // F
         
-        //sarung penghapus segitiga kanan bawah
-        -1, 0, 0.737, 0.682,0.580,
-        -0.95, 0.33, 0.737, 0.682,0.580,
-        -0.6, 0, 0.737, 0.682,0.580,
+        -0.9262413741382, -0.0801076682883  ,0.27, 0.26, 0.26,     // D
+        -0.7471120316662, 0.1049398953577   ,0.27, 0.26, 0.26,     // G
+        -0.7875877998883,-0.0785572394615   ,0.27, 0.26, 0.26,     // Q
+        
+        // DEPAN KARET PENGHAPUS KIRI
+        -0.8976517125306, -0.1812864629679  ,0.15, 0.13, 0.15,     // P
+        -0.7875877998883, -0.0785572394615  ,0.15, 0.13, 0.15,     // Q
+        -0.9262413741382, -0.0801076682883  ,0.15, 0.13, 0.15,     // D
+        
+        // WADAH BAWAH KARET PENGHAPUS KIRI
+        -0.7643422773141, -0.1882573077053  ,0.22, 0.21, 0.21,     // O
+        -0.0931475102232, -0.084497808092   ,0.22, 0.21, 0.21,     // M
+        -0.1346954519298, -0.1916907364028  ,0.22, 0.21, 0.21,     // N
 
-        //sarung penghapus bagian bawah
-        -1,0, 0.588, 0.549, 0.509,
-        -0.6, 0, 0.588, 0.549, 0.509,
-        -0.95, -0.2, 0.588, 0.549, 0.509,
+        -0.7643422773141, -0.1882573077053  ,0.22, 0.21, 0.21,     // O
+        -0.0931475102232, -0.084497808092   ,0.22, 0.21, 0.21,     // M
+        -0.7873177740414, -0.084497808092   ,0.22, 0.21, 0.21,     // E
+        
+        -0.8976517125306, -0.1812864629679  ,0.15, 0.13, 0.15,     // P
+        -0.7875877998883, -0.0785572394615  ,0.15, 0.13, 0.15,     // Q
+        -0.7668961997217, -0.1812864629679  ,0.15, 0.13, 0.15,     // R
+        
+        // WADAH ATAS PENGHAPUS KIRI CREAM
+        -0.7873177740414, -0.084497808092   ,0.81, 0.79, 0.64,     // E
+        -0.1076044909627, -0.0278150182258  ,0.81, 0.79, 0.64,     // J
+        -0.0931475102232, -0.084497808092   ,0.81, 0.79, 0.64,     // M
+        
+        -0.7873177740414, -0.084497808092   ,0.81, 0.79, 0.64,     // E
+        -0.1076044909627, -0.0278150182258  ,0.81, 0.79, 0.64,     // J
+        -0.7799438920148, -0.0278150182258  ,0.81, 0.79, 0.64,     // I
+        
+        // WADAH ATAS PENGHAPUS KIRI MERAH
+        -0.7799438920148, -0.0278150182258  ,0.87, 0.08, 0.02,     //I
+        -0.1347264625551, 0.0321388137151   ,0.87, 0.08, 0.02,     // K
+        -0.1076044909627, -0.0278150182258  ,0.87, 0.08, 0.02,     // J
+        
+        -0.7799438920148, -0.0278150182258  ,0.87, 0.08, 0.02,     // I
+        -0.1347264625551, 0.0321388137151   ,0.87, 0.08, 0.02,     // K
+        -0.7656691701241, 0.0378487024714   ,0.87, 0.08, 0.02,     // H
+        
+        // WADAH ATAS PENGHAPUS KIRI HITAM
+        -0.7656691701241, 0.0378487024714   ,0.09, 0.08, 0.07,     // H
+        -0.1661308507146, 0.0978025344124   ,0.09, 0.08, 0.07,     // L
+        -0.1347264625551, 0.0321388137151   ,0.09, 0.08, 0.07,     // K
 
-        -0.32, -0.2, 0.588, 0.549, 0.509,
-        -0.6, 0, 0.588, 0.549, 0.509,
-        -0.95, -0.2, 0.588, 0.549, 0.509,
-
-        -0.26, 0, 0.588, 0.549, 0.509,
-        -0.6, 0, 0.588, 0.549, 0.509,
-        -0.32, -0.2, 0.588, 0.549, 0.509,
-
-         //penghapus bagian bawah
-         0, 0, 0, 0, 0,
-         -0.081, -0.19, 0, 0, 0,
-         -0.26, 0, 0, 0 , 0,
- 
-         -0.26, 0, 0, 0, 0,
-         -0.081, -0.19, 0, 0, 0,
-         -0.32, -0.194, 0, 0, 0
+        -0.7656691701241, 0.0378487024714   ,0.09, 0.08, 0.07,     // H
+        -0.1661308507146, 0.0978025344124   ,0.09, 0.08, 0.07,     // L
+        -0.7471120316662, 0.1049398953577   ,0.09, 0.08, 0.07,     // G
     ];
 
-    var rightVertices = [
-        //bagian atas
-        // segitiga 1 hitam kiri
-        0, 0, 0.235, 0.235, 0.196, 
-        0.062, 0.3,  0.235, 0.235, 0.196,
-        0.29, 0.3,  0.235, 0.235, 0.196, 
+    var vertices2 = [
+        // ATAS KARET KARET PENGHAPUS KANAN
+        0.4464392048147, -0.1461525931796   ,0.27, 0.26, 0.26,   // U
+        0.8243889555768, -0.1303045620828   ,0.27, 0.26, 0.26,   // C1
+        0.5331657998223, -0.2661149798116   ,0.27, 0.26, 0.26,   // B1
+        
+        0.4464392048147, -0.1461525931796   ,0.27, 0.26, 0.26,   // U
+        0.8243889555768, -0.1303045620828   ,0.27, 0.26, 0.26,   // C1
+        0.7198876905941, -0.0239374098729   ,0.27, 0.26, 0.26,   // E1
+        
+        // KIRI BAWAH KARET PENGHAPUS KANAN
+        0.4464392048147, -0.1461525931796   ,0.15, 0.13, 0.15,   // U
+        0.5219188033328, -0.366871906005    ,0.15, 0.13, 0.15,   // A1
+        0.4400706306713, -0.2433451746829   ,0.15, 0.13, 0.15,   // W
+        
+        0.4464392048147, -0.1461525931796   ,0.15, 0.13, 0.15,   // U
+        0.5219188033328, -0.366871906005    ,0.15, 0.13, 0.15,   // A1
+        0.5331657998223, -0.2661149798116   ,0.15, 0.13, 0.15,   // B1
+        
+        // BAWAH KARET PENGHAPUS KANAN
+        0.5331657998223, -0.2661149798116   ,0.15, 0.13, 0.15,   // B1
+        0.7936663176521, -0.2283174086448   ,0.15, 0.13, 0.15,   // D1
+        0.5219188033328, -0.366871906005    ,0.15, 0.13, 0.15,   // A1
+        
+        0.5331657998223, -0.2661149798116   ,0.15, 0.13, 0.15,   // B1
+        0.7936663176521, -0.2283174086448   ,0.15, 0.13, 0.15,   // D1
+        0.8243889555768, -0.1303045620828   ,0.15, 0.13, 0.15,   // C1
+        
+        // WADAH KIRI PENGHAPUS KANAN
+        0.2020284865347, 0.1263009505974    ,0.22, 0.21, 0.21,   // G1
+        0.4461003062609, -0.1510366192156   ,0.22, 0.21, 0.21,   // V
+        0.4399133039241, -0.2462653533512   ,0.22, 0.21, 0.21,   // Z
+        
+        0.2020284865347, 0.1263009505974    ,0.22, 0.21, 0.21,   // G1
+        0.4461003062609, -0.1510366192156   ,0.22, 0.21, 0.21,   // V
+        0.1828440406819, 0.2397174674433    ,0.22, 0.21, 0.21,   // H1
+        
+        // WADAH ATAS PENGHAPUS KANAN HITAM
+        0.277822748174, 0.2718038447        ,0.09, 0.08, 0.07,   // I1
+        0.4461003062609, -0.1510366192156   ,0.09, 0.08, 0.07,   // V
+        0.1828440406819, 0.2397174674433    ,0.09, 0.08, 0.07,   // H1
 
-        //segitita 2 hitam kanan
-        0,0,0.235, 0.235, 0.196,
-        0.26, 0, 0.235, 0.235, 0.196,
-        0.29, 0.3, 0.235, 0.235, 0.196,
+        0.277822748174, 0.2718038447        ,0.09, 0.08, 0.07,   // I1
+        0.4461003062609, -0.1510366192156   ,0.09, 0.08, 0.07,   // V
+        0.5782564279559, -0.0791197755015   ,0.09, 0.08, 0.07,   // L1
+        
+        // WADAH ATAS PENGHAPUS KANAN MERAH
+        0.5782564279559, -0.0791197755015   ,0.87, 0.08, 0.02,   // L1
+        0.3483014847496, 0.2913425043447    ,0.87, 0.08, 0.02,   // J1
+        0.277822748174, 0.2718038447        ,0.87, 0.08, 0.02,   // I1   
 
-         //sarung penghapus segitiga kiri bawah
-         0.29, 0.33, 0.737, 0.682,0.580,
-         0.26, 0, 0.737, 0.682,0.580,
-         0.6, 0, 0.737, 0.682,0.580,
+        0.5782564279559, -0.0791197755015   ,0.87, 0.08, 0.02,   // L1
+        0.3483014847496, 0.2913425043447    ,0.87, 0.08, 0.02,   // J1
+        0.6674990281567, -0.0413632908011   ,0.87, 0.08, 0.02,   // M1
+        
+        // WADAH ATAS PENGHAPUS KANAN CREAM
+        0.6674990281567, -0.0413632908011   ,0.81, 0.79, 0.64,   // M1
+        0.388774422585, 0.3011118341671     ,0.81, 0.79, 0.64,   // K1
+        0.3483014847496, 0.2913425043447    ,0.81, 0.79, 0.64,   // J1
 
-        // sarung penghapus segitiga atas
-        0.29, 0.33, 0.737, 0.682,0.580,
-         0.95, 0.33, 0.737, 0.682,0.580,
-         0.6, 0, 0.737, 0.682,0.580,
-
-        // sarung penghapus segitiga bawah
-         1, 0, 0.737, 0.682,0.580,
-         0.95, 0.33, 0.737, 0.682,0.580,
-         0.6, 0, 0.737, 0.682,0.580,
-
-        //sarung penghapus bagian bawah
-        1, 0, 0.588, 0.549, 0.509,
-        0.6, 0, 0.588, 0.549, 0.509,
-        0.95, -0.2, 0.588, 0.549, 0.509,
-
-        0.32, -0.2, 0.588, 0.549, 0.509,
-        0.6, 0, 0.588, 0.549, 0.509,
-        0.95, -0.2, 0.588, 0.549, 0.509,
-
-        0.26, 0, 0.588, 0.549, 0.509,
-        0.6, 0, 0.588, 0.549, 0.509,
-        0.32, -0.2, 0.588, 0.549, 0.509,
-
-        //penghapus bagian bawah
-        0, 0, 0, 0, 0,
-        0.081, -0.19, 0, 0, 0,
-        0.26, 0, 0, 0 , 0,
-
-        0.26, 0, 0, 0, 0,
-        0.081, -0.19, 0, 0, 0,
-        0.32, -0.194, 0, 0, 0
+        0.6674990281567, -0.0413632908011   ,0.81, 0.79, 0.64,   // M1
+        0.388774422585, 0.3011118341671     ,0.81, 0.79, 0.64,   // K1
+        0.7242462902585, -0.0214738535408   ,0.81, 0.79, 0.64,   // P1
     ];
+    
+    var vertices = vertices1.concat(vertices2);
 
-    var vertices = [...leftVertices,...rightVertices]; 
-
-    // Create a linked-list for storing the vertices data
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
@@ -114,10 +132,10 @@ function main(){
     var vertexShaderSource = `
         attribute vec2 aPosition;
         attribute vec3 aColor;
-        varying  vec3 vColor;
-        uniform mat4 uTranslate;
-        void main(){
-            gl_Position = uTranslate * vec4(aPosition, 0.0, 1.0); // Center of the coordinate
+        varying vec3 vColor;
+        uniform mat4 uTransformationMatrix;
+        void main() {
+            gl_Position = uTransformationMatrix * vec4(aPosition, 0.0, 1.0);
             vColor = aColor;
         }
     `;
@@ -125,79 +143,100 @@ function main(){
     var fragmentShaderSource = `
         precision mediump float;
         varying vec3 vColor;
-        void main(){
-            gl_FragColor = vec4(vColor, 1.0);
+        void main() {
+            gl_FragColor = vec4(vColor, 1.0);    // Yellow
         }
     `;
-    
-    // Create .c in GPU
+
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderSource);
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader,fragmentShaderSource);
-    
-    // Compile .c into .o
+    gl.shaderSource(fragmentShader, fragmentShaderSource);
+
     gl.compileShader(vertexShader);
     gl.compileShader(fragmentShader);
-    
-    // Prepare a .exe shell (shader program)
+
     var shaderProgram = gl.createProgram();
 
-     // Put the two .o files into the shell
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
-    
-    // Link the two .o files, so together they can be a runnable program/context.
-    gl.linkProgram(shaderProgram);
-    // Start using the context (analogy: start using the paints and the brushes)
-    gl.useProgram(shaderProgram);
-    
-    // Teach the computer how to collect
-    // the positional values from ARRAY_BUFFER
-    // to each vertex being processed
-    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5*Float32Array.BYTES_PER_ELEMENT, 0);
-    gl.enableVertexAttribArray(aPosition);
 
+    gl.linkProgram(shaderProgram);
+
+    gl.useProgram(shaderProgram);
+
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(
+        aPosition, 
+        2, 
+        gl.FLOAT, 
+        false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        0
+    );
+    gl.enableVertexAttribArray(aPosition);
     var aColor = gl.getAttribLocation(shaderProgram, "aColor");
-    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5*Float32Array.BYTES_PER_ELEMENT, 2*Float32Array.BYTES_PER_ELEMENT);
+    gl.vertexAttribPointer(
+        aColor, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        2 * Float32Array.BYTES_PER_ELEMENT
+    );
     gl.enableVertexAttribArray(aColor);
 
-    var speed = 0.0111;
-    var dy = 0;
-    // Create a uniform to animate the vertices
-    const uTranslate = gl.getUniformLocation(shaderProgram, 'uTranslate');
+    var freeze = false;
+
+    const btn = document.getElementById("myButton");
+
+    btn.addEventListener("click", ()=>{
+        freeze = !freeze;
+        if (btn.innerText === "Move"){
+            btn.innerText = "Stop";
+            btn.style.backgroundColor = "#FF2626";
+        } else {
+            btn.innerText= "Move";
+            btn.style.backgroundColor = "#66DE93";
+        }
+    });
+
+    var uTransformationMatrix = gl.getUniformLocation(shaderProgram, 'uTransformationMatrix');
     
+    var speed = 0.0132;
+    var ty = 0;
+
     function render() {
-        //control the bouncing range
-        if (dy >= 0.65 || dy <= -0.65) speed = -speed;
-		dy += speed;
-        
-        const rightPosition = [
-		1.0, 0.0, 0.0, 0.0,
-		0.0, 1.0, 0.0, 0.0,
-		0.0, 0.0, 1.0, 0.0,
-		0, dy, 0.0, 1.0,
-	]
-
-	const leftPosition = [
-		1.0, 0.0, 0.0, 0.0,
-		0.0, 1.0, 0.0, 0.0,
-		0.0, 0.0, 1.0, 0.0,
-		0, 0.0, 0.0, 1.0,
-	]
-		
-        //coloring canvas
-	gl.clearColor(0.7, 0.6, 0.6, 0.8); 
-	gl.clear(gl.COLOR_BUFFER_BIT);
-
-        gl.uniformMatrix4fv(uTranslate, false, leftPosition);
-        gl.drawArrays(gl.TRIANGLES, 0, leftVertices.length/5);
-
-		gl.uniformMatrix4fv(uTranslate, false, rightPosition);
-        gl.drawArrays(gl.TRIANGLES, leftVertices.length/5, rightVertices.length/5);
+        if (!freeze){
+            if (ty >= 0.57 || ty <= -0.58) {
+                speed = -speed;
+            }
+            ty += speed;
             
+            var translationMatrixLeft = new Float32Array([
+                1.0, 0.0, 0.0, 0.0,
+                0.0, 1.0, 0.0, 0.0,
+                0.0, 0.0, 1.0, 0.0,
+                0.0, 0.0, 0.0, 1.0,
+            ]);
+
+            var translationMatrixRight = new Float32Array([
+	        	1.0, 0.0, 0.0, 0.0,
+	        	0.0, 1.0, 0.0, 0.0,
+	        	0.0, 0.0, 1.0, 0.0,
+	        	0.0, ty , 0.0, 1.0,
+	        ]);
+         
+	        gl.clearColor(0.11, 0.05, 0.36, 1.0);
+	        gl.clear(gl.COLOR_BUFFER_BIT);
+        
+            gl.uniformMatrix4fv(uTransformationMatrix, false, translationMatrixLeft);    
+            gl.drawArrays(gl.TRIANGLES, 0, vertices1.length/5);
+        
+	    gl.uniformMatrix4fv(uTransformationMatrix, false, translationMatrixRight);
+            gl.drawArrays(gl.TRIANGLES, vertices1.length/5, vertices2.length/5);
+        }
         requestAnimationFrame(render);
-    }
-    render();
+    }   
+    requestAnimationFrame(render);
 }
